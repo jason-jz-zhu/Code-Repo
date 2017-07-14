31b4f47baa00c9a275064cf60fb11d2cc5fa6b08
Jiazhen Zhu <jason.jz.zhu@gmail.com>
2017-07-14 16:23:43 -0400

    add 15

--- python/15. 3Sum.py
+++ python/15. 3Sum.py
@@ -4,34 +4,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        Solution.res = []
         if nums is None or len(nums) < 3:
-            return Solution.res
+            return []
 
         nums.sort()
-
+        res = []
         for i in xrange(len(nums) - 2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
+
             target = -nums[i]
             left, right = i + 1, len(nums) - 1
-            self.two_sum(nums, left, right, target)
-        return Solution.res
-
-    def two_sum(self, nums, left, right, target):
-        while (left < right):
-            sum = nums[left] + nums[right]
-            if sum == target:
-                Solution.res.append([-target, nums[left], nums[right]])
-                left += 1
-                right -= 1
-                while left < right and nums[left] == nums[left - 1]:
+            while left < right:
+                s = nums[left] + nums[right]
+                if s == target:
+                    res.append([nums[i], nums[left], nums[right]])
                     left += 1
-                while left < right and nums[right] == nums[right + 1]:
                     right -= 1
-            elif sum > target:
-                right -= 1
-            else:
-                left += 1
-
-            
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+                elif s > target:
+                    right -= 1
+                else:
+                    left += 1
+        return res
