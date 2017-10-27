a6a0cf484bddb212ed03a77a3e998ec225e31358
Jiazhen Zhu <jason.jz.zhu@gmail.com>
2017-10-27 19:33:58 -0400

    10/27/2017

--- python/67. Add Binary.py
+++ python/67. Add Binary.py
@@ -10,22 +10,20 @@ class Solution(object):
         if len(a) == 0 or len(b) == 0:
             return a if len(a) != 0 else b
 
-        idxA = len(a) - 1
-        idxB = len(b) - 1
-
-        carry = 0
+        index_a, index_b = len(a) - 1, len(b) - 1
         res = ''
-        while idxA >= 0 or idxB >= 0:
-            tmpA = int(a[idxA]) if idxA >= 0 else 0
-            tmpB = int(b[idxB]) if idxB >= 0 else 0
-            reminder = (tmpA + tmpB + carry) % 2
-            carry = (tmpA + tmpB + carry) / 2
-            if reminder == 0:
-                res = '0' + res
-            else:
-                res = '1' + res
-            idxA -= 1
-            idxB -= 1
-        if carry == 1:
-            res = '1' + res
+        carry = 0
+
+        while index_a >= 0 or index_b >= 0 or carry:
+            if index_a >= 0:
+                carry += int(a[index_a])
+                index_a -= 1
+
+            if index_b >= 0:
+                carry += int(b[index_b])
+                index_b -= 1
+
+            reminder = carry % 2
+            carry = carry / 2
+            res = str(reminder) + res
         return res
