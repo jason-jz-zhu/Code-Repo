52015de7a80be4d1ba0d8237fe4cc1d0b49a7f11
Jiazhen Zhu <jason.jz.zhu@gmail.com>
2019-01-10 14:27:58 -0500

    1/10/2019

--- sql/Advanced SQL/PercentToTotal.sql
+++
@@ -1,5 +0,0 @@
-SELECT a1.Name, a1.Sales, a1.Sales/(SELECT SUM(Sales) FROM Total_Sales) Pct_To_Total
-FROM Total_Sales a1, Total_Sales a2
-WHERE a1.Sales <= a2.sales
-GROUP BY a1.Name, a1.Sales
-ORDER BY a1.Sales DESC, a1.Name DESC;
