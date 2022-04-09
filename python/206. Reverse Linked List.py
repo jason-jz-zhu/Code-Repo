489a540f697018006a2fa050f1b2ba45093b249b
Jiazhen Zhu <jason.jz.zhu@gmail.com>
2022-04-09 10:01:38 -0400

    Update 206. Reverse Linked List.py

--- python/206. Reverse Linked List.py
+++ python/206. Reverse Linked List.py
@@ -22,6 +22,16 @@ class Solution(object):
         return prev
 
 class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        
+        last = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return last
+    
+class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         if not head:
             return None
