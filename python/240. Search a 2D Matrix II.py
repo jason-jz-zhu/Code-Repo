9459e98f5e1bbc94c04e15877f7dc0dfc8fa638e
Jiazhen Zhu <jason.jz.zhu@gmail.com>
2022-04-12 18:22:04 -0400

    Update 240. Search a 2D Matrix II.py

--- python/240. Search a 2D Matrix II.py
+++ python/240. Search a 2D Matrix II.py
@@ -20,3 +20,27 @@ class Solution:
             else:
                 col -= 1
         return False
+
+ class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
+        
+        def search(left, up, right, down):
+            if left > right or up > down:
+                return False
+            elif target < matrix[up][left] or target > matrix[down][right]:
+                return False
+            
+            mid = left + (right - left) // 2
+            
+            row = up
+            while row <= down and matrix[row][mid] <= target:
+                if matrix[row][mid] == target:
+                    return True
+                row += 1
+                
+            return search(left, row, mid - 1, down) or search(mid + 1, up, right, row - 1)
+        
+        
+        return search(0, 0, len(matrix[0]) - 1, len(matrix) - 1)
