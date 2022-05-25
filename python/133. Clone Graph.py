b05b6dcd26b3de7baf7bc883914363dbb558e8c4
Jiazhen Zhu <jason.jz.zhu@gmail.com>
2022-05-25 14:34:11 -0400

    Update 133. Clone Graph.py

--- python/133. Clone Graph.py
+++ python/133. Clone Graph.py
@@ -1,41 +1,79 @@
-# Definition for a undirected graph node
-# class UndirectedGraphNode:
-#     def __init__(self, x):
-#         self.label = x
-#         self.neighbors = []
-
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+# dfs
 class Solution:
-    # @param node, a undirected graph node
-    # @return a undirected graph node
-    def cloneGraph(self, node):
-        root = node
-        if node is None:
-            return node
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return None
+        
+        def dfs(n):
+            if n in visited:
+                return
+            if n not in graph:
+                graph[n] = Node(n.val)
+            visited.add(n)
+            for nxt in n.neighbors:
+                if nxt not in graph:
+                    graph[nxt] = Node(nxt.val)
+                graph[n].neighbors.append(graph[nxt])
+                dfs(nxt)
+        
+        
+        graph = {}
+        visited = set()
+        dfs(node)
+        return graph[node]
 
-        # using bfs to get all nodes
-        nodes = self.getNodes(node)
-
-        # using hashset to map from old to new node
-        mapping = {}
-        for node in nodes:
-            mapping[node] = UndirectedGraphNode(node.label)
-
-        # copy neighbors
-        for node in nodes:
-            new_node = mapping[node]
-            for neighbor in node.neighbors:
-                new_neighbor = mapping[neighbor]
-                new_node.neighbors.append(new_neighbor)
-
-        return mapping[root]
-
-    def getNodes(self, node):
-        q = collections.deque([node])
-        result = set([node])
+# dfs
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return None
+        
+        graph = {}
+        visited = set()
+        stack = [node]
+        
+        while stack:
+            n = stack.pop()
+            if n in visited:
+                continue
+            if n not in graph:
+                graph[n] = Node(n.val)
+            visited.add(n)
+            for nxt in n.neighbors:
+                if nxt not in graph:
+                    graph[nxt] = Node(nxt.val)
+                graph[n].neighbors.append(graph[nxt])
+                stack.append(nxt)
+        return graph[node]
+            
+# bfs
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return None
+        
+        graph = {}
+        visited = set()
+        q = deque([node])
+        
         while q:
-            head = q.popleft()
-            for neighbor in head.neighbors:
-                if neighbor not in result:
-                    result.add(neighbor)
-                    q.append(neighbor)
-        return result
+            n = q.popleft()
+            if n in visited:
+                continue
+            if n not in graph:
+                graph[n] = Node(n.val)
+            visited.add(n)
+            for nxt in n.neighbors:
+                if nxt not in graph:
+                    graph[nxt] = Node(nxt.val)
+                graph[n].neighbors.append(graph[nxt])
+                q.append(nxt)
+        return graph[node]
+        
