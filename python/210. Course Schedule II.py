fb600b83d382e59e77e15c27468dae7003d11089
Jiazhen Zhu <jason.jz.zhu@gmail.com>
2022-05-27 06:37:10 -0400

    Update 210. Course Schedule II.py

--- python/210. Course Schedule II.py
+++ python/210. Course Schedule II.py
@@ -1,35 +1,22 @@
-class Solution(object):
-    def findOrder(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: List[int]
-        """
-        if prerequisites is None:
-            return []
-
-        degree = [0 for _ in range(numCourses)]
-        graph = {i: set() for i in range(numCourses)}
+# topological
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        indegree = [0 for _ in range(numCourses)]
         for end, start in prerequisites:
-            if end not in graph[start]:
-                degree[end] += 1
-            graph[start].add(end)
-
-        return self.bfs(degree, graph)
-
-    def bfs(self, degree, graph):
-        res = []
-        q = collections.deque([i for i in range(len(degree)) if degree[i] == 0])
+            indegree[end] += 1
+            graph[start].append(end)
+        
+        q = deque([node for node in range(numCourses) if indegree[node] == 0])
+        ans = []
         while q:
-            c = q.popleft()
-            res.append(c)
-            for node in graph[c]:
-                degree[node] -= 1
-                if degree[node] == 0:
-                    q.append(node)
-        if len(res) == len(degree):
-            return res
-        return []
+            node = q.popleft()
+            ans.append(node)
+            for nxt in graph[node]:
+                indegree[nxt] -= 1
+                if indegree[nxt] == 0:
+                    q.append(nxt)
+        return ans if len(ans) == numCourses else None
 
 
 # dfs
