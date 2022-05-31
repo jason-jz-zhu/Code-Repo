6a34522276367bb52dacdf926484de3c6459e8a6
Jiazhen Zhu <jason.jz.zhu@gmail.com>
2022-05-31 13:34:55 -0400

    Update 905. Sort Array By Parity.py

--- python/905. Sort Array By Parity.py
+++ python/905. Sort Array By Parity.py
@@ -1,12 +1,9 @@
 class Solution:
-    def sortArrayByParity(self, A: List[int]) -> List[int]:
-        if not A or len(A) == 0:
-            return []
-
-        start = end = 0
-        while end < len(A):
-            if A[end] % 2 == 0:
-                A[start], A[end] = A[end], A[start]
-                start += 1
-            end += 1
-        return A
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        left = right = 0
+        while right < len(nums):
+            if nums[right] % 2 == 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            right += 1
+        return nums
