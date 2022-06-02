0eabf6afdcb51530fe2db95aa861e3ef1c30e501
Jiazhen Zhu <jason.jz.zhu@gmail.com>
2022-06-02 10:42:12 -0400

    Update 67. Add Binary.py

--- python/67. Add Binary.py
+++ python/67. Add Binary.py
@@ -1,29 +1,20 @@
-class Solution(object):
-    def addBinary(self, a, b):
-        """
-        :type a: str
-        :type b: str
-        :rtype: str
-        """
-        if a is None or b is None:
-            return ''
-        if len(a) == 0 or len(b) == 0:
-            return a if len(a) != 0 else b
-
-        index_a, index_b = len(a) - 1, len(b) - 1
-        res = ''
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
         carry = 0
-
-        while index_a >= 0 or index_b >= 0 or carry:
-            if index_a >= 0:
-                carry += int(a[index_a])
-                index_a -= 1
-
-            if index_b >= 0:
-                carry += int(b[index_b])
-                index_b -= 1
-
+        idx_a, idx_b = len(a) - 1, len(b) - 1
+        ans = ''
+        while idx_a >= 0 or idx_b >= 0:
+            if idx_a >= 0:
+                carry += int(a[idx_a])
+                idx_a -= 1
+            if idx_b >= 0:
+                carry += int(b[idx_b])
+                idx_b -= 1
+            
             reminder = carry % 2
-            carry = carry / 2
-            res = str(reminder) + res
-        return res
+            carry //= 2
+            ans = str(reminder) + ans
+        if carry:
+            ans = str(carry) + ans
+        return ans
+            
