462444f4f448fedc6b04aa69d2bae5b2328cdf9e
Jiazhen Zhu <jason.jz.zhu@gmail.com>
2022-06-07 07:50:53 -0400

    Update 136. Single Number.py

--- python/136. Single Number.py
+++ python/136. Single Number.py
@@ -1,3 +1,16 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hashmap = defaultdict(int)
+        for num in nums:
+            hashmap[num] += 1
+        for key, val in hashmap.items():
+            if val == 1:
+                return key
+            
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return 2 * sum(set(nums)) - sum(nums)
+
 class Solution(object):
     def singleNumber(self, nums):
         """
