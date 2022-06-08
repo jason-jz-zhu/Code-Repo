3beaca75a4d18a6ea8733beab1a2f564783ae5f6
Jiazhen Zhu <jason.jz.zhu@gmail.com>
2022-06-08 19:32:59 -0400

    Update 69. Sqrt(x).py

--- python/69. Sqrt(x).py
+++ python/69. Sqrt(x).py
@@ -1,4 +1,21 @@
 class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        start, end = 1, x
+        while start <= end:
+            mid = start + (end - start) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                start = mid + 1
+            else:
+                end = mid - 1
+        return end if end * end < x else start
+
+
+
+class Solution:
     def mySqrt(self, x):
         """
         :type x: int
