9e29f648d723fe7a26f60befc3815e1cd4a1153b
Jiazhen Zhu <jason.jz.zhu@gmail.com>
2022-06-08 07:32:03 -0400

    Update 359. Logger Rate Limiter.py

--- python/359. Logger Rate Limiter.py
+++ python/359. Logger Rate Limiter.py
@@ -1,32 +1,14 @@
-class Logger(object):
+class Logger:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self._hashmap = {}
+        self.hashmap = defaultdict(int)
+        self.range = 10
 
-
-    def shouldPrintMessage(self, timestamp, message):
-        """
-        Returns true if the message should be printed in the given timestamp, otherwise returns false.
-        If this method returns false, the message will not be printed.
-        The timestamp is in seconds granularity.
-        :type timestamp: int
-        :type message: str
-        :rtype: bool
-        """
-
-        if message not in self._hashmap:
-            self._hashmap[message] = timestamp
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        if message not in self.hashmap:
+            self.hashmap[message] = timestamp + self.range
             return True
-        elif timestamp -self._hashmap[message] >= 10:
-            self._hashmap[message] = timestamp
+        if self.hashmap[message] <= timestamp:
+            self.hashmap[message] = timestamp + self.range
             return True
-        else:
-            return False
-
-
-# Your Logger object will be instantiated and called as such:
-# obj = Logger()
-# param_1 = obj.shouldPrintMessage(timestamp,message)
+        return False
