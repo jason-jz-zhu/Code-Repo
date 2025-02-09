0d8859a12c21b396e0cce331a8624929fe95f6bc
Jiazhen Zhu <jason.jz.zhu@gmail.com>
2025-02-09 09:30:15 -0500

    update 3 and 438

--- python/3. Longest Substring Without Repeating Characters.py
+++ python/3. Longest Substring Without Repeating Characters.py
@@ -1,5 +1,24 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        left = right = 0
+        windows = {}
+        res = 0
+        while right < len(s):
+            c = s[right]
+            right += 1
+            windows[c] = 1 if c not in windows else windows[c] + 1
+            # windows[c] = windows.get(c, 0) + 1
+            while windows[c] > 1:
+                d = s[left]
+                left += 1
+                windows[d] -= 1
+
+            res = max(res, right - left)
+
+        return res
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
         visited = collections.defaultdict(int)
         left = right = 0
         res=  0
@@ -46,3 +65,39 @@ class Solution:
                 visited.remove(s[left])
                 left += 1
         return res
+
+
+# Pseudocode framework for sliding window algorithm
+def slidingWindow(s: str):
+    # Use an appropriate data structure to record the data
+    # in the window, which can vary depending on the
+    # For example, if I want to record the
+    # frequency of elements in the window, I would
+    # If I want to record the sum of elements in the window, I could just use an int
+    window = ...
+
+    left, right = 0, 0
+    while right < len(s):
+        # c is the character that will be added to the window
+        c = s[right]
+        window.add(c)
+        # Expand the window
+        right += 1
+        # Perform a series of updates on the data within the window
+        ...
+
+        # *** position for debug output ***
+        # Note that you should not print in the final solution code
+        # because IO operations are time-consuming and may cause timeouts
+        # print(f"window: [{left}, {right})")
+        # ***********************
+
+        # Determine whether the left side of the window needs to be contracted
+        while left < right and window needs shrink:
+            # d is the character that will be removed from the window
+            d = s[left]
+            window.remove(d)
+            # Shrink the window
+            left += 1
+            # Perform a series of updates on the data within the window
+            ...
--- python/438. Find All Anagrams in a String.py
+++ python/438. Find All Anagrams in a String.py
@@ -1,3 +1,33 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        window = {}
+        need = {}
+        for c in p:
+            need[c] = need.get(c, 0) + 1
+        left = right = 0
+        valid = 0
+        res = []
+
+        while right < len(s):
+            c = s[right]
+            right += 1
+            if c in need:
+                window[c] = window.get(c, 0) + 1
+                if window[c] == need[c]:
+                    valid += 1
+            while right - left >= len(p):
+                if valid == len(need):
+                    res.append(left)
+                d = s[left]
+                left += 1
+                if d in need:
+                    if window[d] == need[d]:
+                        valid -= 1
+                    window[d] -= 1
+        return res
+
+
+
 class Solution(object):
     def findAnagrams(self, s, p):
         """
