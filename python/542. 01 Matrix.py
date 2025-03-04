cb769670ed42c83aaf34c6fdde94930da8f74e79
Jiazhen Zhu <jason.jz.zhu@gmail.com>
2025-03-04 07:00:25 -0500

    Update 542. 01 Matrix.py

--- python/542. 01 Matrix.py
+++ python/542. 01 Matrix.py
@@ -1,3 +1,31 @@
+# 2025
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        res = [[-1 for _ in range(n)] for _ in range(m)]
+
+        q = collections.deque()
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 0:
+                    q.append((i, j))
+                    res[i][j] = 0
+
+        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        while q:
+            x, y = q.popleft()
+            for dx, dy in dirs:
+                nextX, nextY = x + dx, y + dy
+                if 0 <= nextX < m and 0 <= nextY < n and res[nextX][nextY] == -1:
+                    q.append((nextX, nextY))
+                    res[nextX][nextY] = res[x][y] + 1
+
+        return res
+
+
+
+
+
 class Solution(object):
     def updateMatrix(self, matrix):
         """
